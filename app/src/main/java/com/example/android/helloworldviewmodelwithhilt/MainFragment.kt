387d829fb1f3a06b1c430ffd910e97499657c554
Winsey Li <winsey@storypark.com>
2022-07-21 16:18:24 +1200

    Add a fragment in the MainActivity that share a ViewModel using their activity scope to handle communication

---
+++ app/src/main/java/com/example/android/helloworldviewmodelwithhilt/MainFragment.kt
@@ -0,0 +1,42 @@
+package com.example.android.helloworldviewmodelwithhilt
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.example.android.helloworldviewmodelwithhilt.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+
+    // View Binding
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    // Use the 'by activityViewModels()' Kotlin property delegate
+    // from the fragment-ktx artifact
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getCount().observe(this, Observer<Int>{ count ->
+            // update UI
+            binding.textViewFirst.text = "In the fragment, count: $count"
+        })
+    }
+
+}
